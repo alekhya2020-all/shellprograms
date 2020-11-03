fb258902df62a270057571b42fb6ddee1460e08f
alekhya2020-all <sontialekhya@gmail.com>
2020-11-03 15:18:47 +0530

    monthly wages

--- ForLoopuc5.java
+++ ForLoopuc5.java
@@ -3,12 +3,12 @@ public class ForLoopuc5{
    public static final int PartTime=1;
    public static final int FullTime=2;
    public static final int EmpRateperhour=20;
-	public static final int NumberOfworkingdays=2;
+   public static final int NumberOfworkingdays=2;
 
    public static void main(String[] args){
        int EmpHours=0,  EmpWage=0,  TotalEmployeeWage=0;
        for(int day=0;day<NumberOfworkingdays;day++){
-		 int randomVar=(int)Math.floor(Math.random()*10)%3;
+       int randomVar=(int)Math.floor(Math.random()*10)%3;
        switch (randomVar){
        case PartTime:
             EmpHours=4;
@@ -21,9 +21,9 @@ public class ForLoopuc5{
          }
             EmpWage = EmpHours * EmpRateperhour;
             TotalEmployeeWage += EmpWage;
-		      System.out.println("EmpWage:"+EmpWage);
+            System.out.println("EmpWage:"+EmpWage);
 }
-				System.out.println("TotalEmployeeWage:"+TotalEmployeeWage);
+            System.out.println("TotalEmployeeWage:"+TotalEmployeeWage);
 }
 }
 
