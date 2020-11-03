cec48b1bfa96e73169bfe9fe3fdd696af647810c
alekhya2020-all <sontialekhya@gmail.com>
2020-11-03 14:48:22 +0530

    changes

--- SwitchCaseuc4.java
+++ SwitchCaseuc4.java
@@ -4,22 +4,23 @@ public class SwitchCaseuc4{
 	public static final int FullTime=2;
 	public static final int EmpRateperhour=20;
 
-public static void main(String[] args){
-   int EmpHours=0;
-   int EmpWages=0;
-   double randomVar=Math.floor(Math.random()*10)%3;
-   System.out.println(+randomVar);
-   switch((int)randomVar){
-        case PartTime:
-             EmpHours=4;
-             break;
-        case FullTime:
-             EmpHours=8;
-             break;
-        default:
-        		 EmpHours=0;
-   		}
-				EmpWages = EmpHours * EmpRateperhour;
-				System.out.println("EmpWages:"+EmpWages);
-	}
+	public static void main(String[] args){
+	    int EmpHours=0;
+		 int EmpWage=0;
+		 double randomVar=Math.floor(Math.random()*10%3);
+		 System.out.println("random:"+randomVar);
+		 switch ((int)randomVar){
+		 case PartTime:
+				EmpHours=4;
+				break;
+  		 case FullTime:
+				EmpHours=8;
+				break;
+
+		 default:
+				EmpHours=0;
+			}
+				EmpWage = EmpHours * EmpRateperhour;
+				System.out.println("EmpWage:"+EmpWage);
+}
 }
