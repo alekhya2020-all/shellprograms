a49db19f02b10d1a0c751063ed7ee92720a43bd4
alekhya2020-all <sontialekhya@gmail.com>
2020-11-04 11:56:03 +0530

    method

---
+++ ClassMethoduc7.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class ClassMethoduc7{
+   public static final int PartTime=1;
+   public static final int FullTime=2;
+   public static final int EmpRateperhour=20;
+   public static final int NumberOfworkingdays=2;
+   public static final int Maxhoursinmonth=10;
+	public static int computeEmpWage() {
+       int TotalEmployeeHours=0, TotalEmpWage=0, TotalWorkingDays=0;
+       while (TotalEmployeeHours<=Maxhoursinmonth && TotalWorkingDays < NumberOfworkingdays){
+       int EmpHours=0, EmpWage=0;
+       TotalWorkingDays++;
+       int randomVar=(int)Math.floor(Math.random()*10)%3;
+       switch (randomVar){
+       case PartTime:
+            EmpHours=4;
+            break;
+       case FullTime:
+            EmpHours=8;
+            break;
+       default:
+            EmpHours=0;
+         }
+            TotalEmployeeHours += EmpHours;
+            System.out.println("Day#:" +TotalWorkingDays +"EmpHours:"+EmpHours);
+}
+			   TotalEmpWage=TotalEmployeeHours*EmpRateperhour;
+            System.out.println("TotalEmpWage:"+TotalEmpWage);
+				return TotalEmpWage;
+}
+				public static void main(String[] args){
+				computeEmpWage();
+
+	}
+}
+
+
+
+
