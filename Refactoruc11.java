a966eddcfb346bce0071e46c6d0d72cb7a985869
alekhya2020-all <sontialekhya@gmail.com>
2020-11-05 12:03:46 +0530

    refactor

---
+++ Refactoruc11.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Refactoruc11{
+   public static final int PartTime=1;
+   public static final int FullTime=2;
+   public static int computeEmpWage(String Company,int EmpRateperhour,int NumberOfworkingdays,int Maxhoursinmonth) {
+       int TotalEmployeeHours=0, TotalEmpWage=0, TotalWorkingDays=0;
+       while (TotalEmployeeHours<=Maxhoursinmonth && TotalWorkingDays < NumberOfworkingdays){
+       int EmpHours=0, EmpWage=0;
+       TotalWorkingDays++;
+       int randomVar=(int)Math.floor(Math.random()*10)%3;
+       switch (randomVar){
+       case PartTime:
+            EmpHours=4;
+            break;
+       case FullTime:
+            EmpHours=8;
+            break;
+       default:
+            EmpHours=0;
+         }
+            TotalEmployeeHours += EmpHours;
+            System.out.println("Day#:" +TotalWorkingDays +"EmpHours:"+EmpHours);
+}
+            TotalEmpWage=TotalEmployeeHours*EmpRateperhour;
+            System.out.println("TotalEmpWage:"+TotalEmpWage);
+            return TotalEmpWage;
+}
+            public static void main(String[] args){
+            computeEmpWage("Flipkart" ,25,2,10);
+				computeEmpWage("Amazon" ,15,4,15);
+				}
+}
